92bf8117417c3b935b188b2170830d3063c0057d
Dongha Hwang <depth221@gmail.com>
2021-02-06 14:08:32 +0900

    Update convertphonenumber.py

--- convertphonenumber.py
+++ convertphonenumber.py
@@ -1,5 +1,8 @@
 import csv
 import sys
+import time
+
+start = time.time()
 
 csv_file_path = sys.argv[1]
 
@@ -24,14 +27,18 @@ for line in fr_csv_r: # convert the csv object to a list
 keys_to_add = fr_csv[0]
 
 for j in range(2, len(keys_to_add)):
-    if keys_to_add[j] in ['phone', 'contact:phone', 'contact:mobile', 'contact:fax', 'contact:tty']:
+    if keys_to_add[j] in ['phone', 'fax', 'contact:phone', 'contact:mobile', 'contact:fax', 'contact:tty']:
         # if phone numbers exist
         print("Info: Found the tag '" + keys_to_add[j] + "'")
         
         for i in range(1, len(fr_csv)):
+            if fr_csv[i][j] in ["-"]:
+                fr_csv[i][j] = None
+                continue
+            
             if fr_csv[i][j]: # if the phone number exists
                 phone = "+" + country_code
-                phone_splitted = fr_csv[i][j].split('-')
+                phone_splitted = fr_csv[i][j].strip().split('-')
     
                 if phone_splitted[0][0] == "0": # delete the first '0'
                     phone_splitted[0] = phone_splitted[0][1:]
@@ -39,20 +46,19 @@ for j in range(2, len(keys_to_add)):
                 for phone_factor in phone_splitted: # delete '-' and insert a space
                     phone += " " + phone_factor
                     
-                
-            fr_csv[i][j] = phone
+                fr_csv[i][j] = phone
 
 print("Info: Done converting")
 fr.close()
 try:
     fw = open(csv_file_path[:-4] + "_phone.csv", 'w', encoding=csv_incoding, newline='') # write
 except:
-    print("Error: Please delete '" + csv_file_path[:-4] + "_phone.csv'")
+    print("Error: Please delete '" + csv_file_path[:-4] + "_phone.csv' (%.3fs)" %(time.time() - start))
     sys.exit()
 
 writer = csv.writer(fw)
 for i in fr_csv:
     writer.writerow(i)
 
-print("Info: Writed to '" + csv_file_path[:-4] + "_phone.csv'")
+print("Info: Writed to '" + csv_file_path[:-4] + "_phone.csv' (%.3fs)" %(time.time() - start))
 fw.close()
